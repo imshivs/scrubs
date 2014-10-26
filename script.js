e30e1524163affe570bf21dc953e5f87e5172b4a
imshivs <shivani.negi@live.com>
2014-10-26 15:26:24 -0400

    make it better

---
+++ script.js
@@ -0,0 +1,19 @@
+SC.initialize({
+    client_id: '718b10dd68233434dfb06e5610561ecb'
+});
+
+// $(document).ready(function(){
+//   SC.connect(function() {
+//     SC.get('/me', function(me) { 
+//       alert('Hello, ' + me.username); 
+//     });
+//   });
+// });
+
+// $(document).ready(function() {
+  SC.get('/tracks', { genres: 'pop' }, function(tracks) {
+    $(tracks).each(function(index, track) {
+      $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+    });
+  });
+// });
